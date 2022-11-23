822384377cb1d07bdde6a9f067e22b23920cd0c4
Naveen Muralidharan <naveenspec@gmail.com>
2022-11-22 19:55:04 -0500

    added index and show route, imported dependencies and model

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 require("dotenv").config()
 const express = require("express")
 const methodOverride = require("method-override")
-
+const pokedex = require("./models/pokemon")
 
 const app = express()
 
@@ -11,7 +11,22 @@ const app = express()
 // Routes
 // index route
 app.get('/pokedex', (req, res)=>{
-    res.send('/pokedex get route')
+    res.render("index.ejs", { pokedex })
 })
+
+// show route
+app.get('/pokedex/:id', (req, res)=>{
+
+    pokedex.forEach((pokemon)=>{
+        if(pokemon.id === req.params.id){
+            res.render("show.ejs", { pokemon })
+        }
+    })
+
+    
+})
+
+
+
 
 app.listen(process.env.PORT, ()=> {console.log("App is running on port " + process.env.PORT)})
